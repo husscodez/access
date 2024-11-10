831ced0c637c61da110a70840afd8be18f2cd9c2
husscodez <huss.healthadvisor@gmail.com>
2024-11-10 14:37:54 -0800

    Update script.js

--- script.js
+++ script.js
@@ -13,10 +13,33 @@ t.onpaste=e=>{
     txtPrimaryW.value=150
     if(txtZipCode.value!=at.value.split('\n')[0]){
       txtZipCode.value=at.value.split('\n')[0]
-      zipCodeChanged()
-      setTimeout('__doPostBack(\'txtZipCode\',\'\')',0)
+      zipCodeChanged();setTimeout('__doPostBack(\'txtZipCode\',\'\')',0)
+			
+			ti=setInterval(function(){
+				if(UpdateProgress10.style.display=='none'){
+					clearInterval(ti)
+					if(ddlAppType.innerText.includes("Fixed Indemnity"))p0()
+					if(ddlAppType.innerText.includes("Premier Choice"))p1()
+					if(ddlAppType.innerText.includes("SecureAdvantage"))p2()
+				}
+			},50)			
     }
 	})
+}
+
+function p0(){
+	if(ddlAppType.value!=25){
+		ddlAppType.value=25
+		getSessionStorageByAppTypeID();setTimeout('__doPostBack(\'ddlAppType\',\'\')', 0)
+	}
+}
+
+function p1(){
+
+}
+
+function p2(){
+
 }
 
 console.log(1010101)
