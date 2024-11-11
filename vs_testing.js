6bb236fe5e6a5b92a82d359bfb5145a601f3db5e
husscodez <huss.healthadvisor@gmail.com>
2024-11-10 18:39:54 -0800

    Update vs_testing.js

--- vs_testing.js
+++ vs_testing.js
@@ -1,6 +1,7 @@
 // https://vanillasoft.net/web/caller/html/caller.asp
 
 console.log(99999)
+alert(9999)
 
 if(!bodyIframe.contentDocument.getElementsByClassName('copy').lengthclientName.insertAdjacentHTML('afterend','<button class="copy" style="position:absolute;right:0px;top:10px;width:10px;padding:0px;max-width:15px;hight:10px;z-index:1;padding:4px" tabindex="0" data-accessibility-tab="true"><img class="copy" src="https://cdn-icons-png.flaticon.com/512/54/54702.png" style="width:100%;max-width:15px;margin:0px;padding:0px;hight:100%;vertical-align: middle;filter: invert(.95);"></button>')
 
