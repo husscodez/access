78da57755d5d10d45a0a178c5216c08bde71e64f
husscodez <huss.healthadvisor@gmail.com>
2024-11-11 17:20:38 -0800

    Update td_copy.js

--- td_copy.js
+++ td_copy.js
@@ -1,10 +1,9 @@
-console.log("919191")
+console.log("222")
 convertDate=date=>date.replace(/(\d{1,2})[\/\-](\d{1,2})[\/\-](\d{2,4})/,(m,d,mth,y)=>`${d.padStart(2,'0')}/${mth.padStart(2,'0')}/${y.length==2?'20'+y:y}`)
 
 console.log(document.getElementById('sel_username'))
 
 onclick=e=>{
-console.log("CLICKED")
 if(e.target.id=='sel_username'){
 c=document.createElement('button')
 c.className='btn btn-info'
@@ -25,7 +24,7 @@ navigator.clipboard.writeText(zip_code.value+'\n'+convertDate(bdv))
 }
 },50)
 }
+if(e.target.id=='threedot')add_tag.click()
 }
 
-threedot.onclick=e=>add_tag.click()
 
