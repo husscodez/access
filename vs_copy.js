a3ec4bc600555984c6c542364f3690b083d95b2b
husscodez <huss.healthadvisor@gmail.com>
2024-11-18 06:08:39 -0800

    Update vs_copy.js

--- vs_copy.js
+++ vs_copy.js
@@ -1,4 +1,4 @@
-console.log("VS loaded")
+console.log("VS loaded!")
 
 function add_copy_btn(){
 bodyIframe.contentDocument.getElementById('clientName').insertAdjacentHTML('afterend','<button class="copy" style="position:absolute;right:0px;top:10px;width:10px;padding:0px;max-width:15px;hight:10px;z-index:1;padding:4px" tabindex="0" data-accessibility-tab="true"><img class="copy" src="https://cdn-icons-png.flaticon.com/512/54/54702.png" style="width:100%;max-width:15px;margin:0px;padding:0px;hight:100%;vertical-align: middle;filter: invert(.95);"></button>')
@@ -34,5 +34,27 @@ if(e.target.innerText=='NC DEFAULT'||e.target.innerText=='No Contact'||e.target.
       if(bodyIframe.contentDocument.getElementsByClassName('notifyButtonWrapper').length)bodyIframe.contentDocument.getElementsByClassName('notifyButtonWrapper')[0].childNodes[0].click()
     },bodyIframe.contentDocument.getElementsByClassName('notificationContent')[0].innerText.split('have ')[1].split(' seconds')[0]*1000)
   },200)
-}
 }}}
+} // on load end
+
+
+let name_hist=""
+document.onmousedown=e=>{
+  console.log("Mouse down **")
+  if(e.target.innerText=='NC DEFAULT'||e.target.innerText=='No Contact'){
+    console.log("NC ***")
+    name_hist_int=setInterval(function(){
+      if((bodyIframe.contentDocument.getElementById('FirstName').value+bodyIframe.contentDocument.getElementById('LastName').value).toLocaleUpperCase()!=name_hist){
+        clearInterval(name_hist_int)
+        setTimeout(function(){
+          name_hist=(bodyIframe.contentDocument.getElementById('FirstName').value+bodyIframe.contentDocument.getElementById('LastName').value).toLocaleUpperCase()
+          if(!bodyIframe.contentDocument.getElementsByClassName('notificationButton notificationButtonBlue notifyButton').length){
+            bodyIframe.contentDocument.getElementById('tcpaShowNumbers').click()
+            bodyIframe.contentDocument.getElementsByClassName('notificationButton notificationButtonBlue notifyButton')[0].click()
+            bodyIframe.contentDocument.getElementsByClassName('dialphone')[0].click()
+          }
+        },1e3)
+      }
+    },500)
+  }
+}
