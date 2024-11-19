f881c241d6a6c0567b3d4a2c832ae529113d13ec
husscodez <huss.healthadvisor@gmail.com>
2024-11-19 10:31:48 -0800

    Update vs_copy.js

--- vs_copy.js
+++ vs_copy.js
@@ -1,4 +1,4 @@
-console.log("VS loaded!")
+console.log("VS loaded")
 
 function add_copy_btn(){
 bodyIframe.contentDocument.getElementById('clientName').insertAdjacentHTML('afterend','<button class="copy" style="position:absolute;right:0px;top:10px;width:10px;padding:0px;max-width:15px;hight:10px;z-index:1;padding:4px" tabindex="0" data-accessibility-tab="true"><img class="copy" src="https://cdn-icons-png.flaticon.com/512/54/54702.png" style="width:100%;max-width:15px;margin:0px;padding:0px;hight:100%;vertical-align: middle;filter: invert(.95);"></button>')
@@ -13,11 +13,12 @@ if(!bodyIframe.contentDocument.getElementsByClassName('copy').length){
     },4e3)
   }
 document.body.onclick=e=>{
-  //console.log("clicked ***") // NOT WORKING - SELECT VAL ON CLICK
+  /*//console.log("clicked ***") // NOT WORKING - SELECT VAL ON CLICK
   if(e.target.id=='searchTerm'){
     //console.log("INPUT clicked ***")
     e.target.select()
-  }
+  }*/
+  console.log(e.target)
   
 if(e.target.className=='copy'){
   navigator.clipboard.writeText(bodyIframe.contentDocument.getElementById('ZipCode').value+'\n'+(bodyIframe.contentDocument.getElementById('n2393619').value.length>3?bodyIframe.contentDocument.getElementById('n2393619').value.replace(/\D/g,''):'01/01/'+(new Date().getYear()+1900-bodyIframe.contentDocument.getElementById('n2393583').value))+'\n')
