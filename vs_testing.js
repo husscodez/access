4c3b3f48a55cda45651c9d9308958487741f9764
husscodez <huss.healthadvisor@gmail.com>
2024-11-25 15:56:34 -0800

    Update vs_testing.js

--- vs_testing.js
+++ vs_testing.js
@@ -1,5 +1,16 @@
 document.body.ondblclick=e=>{
+console.log("***** 111")
+console.log(comments)
 console.log("*****")
+}
+document.ondblclick=e=>{
+console.log("***** 222")
+console.log(comments)
+console.log("*****")
+}
+document.body.contextmenu=e=>{
+e.preventDefault()
+console.log("***** 333")
 console.log(comments)
 console.log("*****")
 }
