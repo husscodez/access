66441e944d896c50f1b8e0f46958588f0e688bfc
husscodez <huss.healthadvisor@gmail.com>
2024-11-26 08:01:36 -0800

    Update VS_navbar.js

--- VS_navbar.js
+++ VS_navbar.js
@@ -1,1 +1,3 @@
-document.getElementsByClassName('navbar')[0].setAttribute('style',"background: linear-gradient(45deg,#000,#007acc,#000);")
+onload=e=>{
+  document.getElementsByClassName('navbar')[0].setAttribute('style',"background: linear-gradient(45deg,#000,#007acc,#000);")
+}
