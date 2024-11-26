b1d92094dcdaa784e0c1b2abbed449a4b3332df0
husscodez <huss.healthadvisor@gmail.com>
2024-11-26 06:04:34 -0800

    Update vs_testing.js

--- vs_testing.js
+++ vs_testing.js
@@ -4,31 +4,36 @@ function add_copy_btn(){
 document.getElementById('clientName').insertAdjacentHTML('afterend','<button class="copy" style="position:absolute;right:0px;top:10px;width:10px;padding:0px;max-width:15px;hight:10px;z-index:1;padding:4px" tabindex="0" data-accessibility-tab="true"><img class="copy" src="https://cdn-icons-png.flaticon.com/512/54/54702.png" style="width:100%;max-width:15px;margin:0px;padding:0px;hight:100%;vertical-align: middle;filter: invert(.95);"></button>')
 }
 
+
+
 onload=e=>{
   console.log("Document loaded ****")
-if(!document.getElementsByClassName('copy').length){
-  add_copy_btn()
-  if(!document.getElementsByClassName('copy').length){
-    setTimeout(function(){
-      if(!document.getElementsByClassName('copy').length){add_copy_btn()}
-      else{
-        setTimeout(function(){
-          if(!document.getElementsByClassName('copy').length){add_copy_btn()}
-          else{
-            setTimeout(function(){
-              if(!document.getElementsByClassName('copy').length){add_copy_btn()}
-            },3e3)
-          }
-        },3e3)
-      }
-    },3e3)
-  }
+	if(!document.getElementsByClassName('copy').length){
+		add_copy_btn()
+		if(!document.getElementsByClassName('copy').length){
+			setTimeout(function(){
+				if(!document.getElementsByClassName('copy').length){add_copy_btn()}
+				else{
+					setTimeout(function(){
+						if(!document.getElementsByClassName('copy').length){add_copy_btn()}
+						else{
+							setTimeout(function(){
+								if(!document.getElementsByClassName('copy').length){add_copy_btn()}
+							},3e3)
+						}
+					},3e3)
+				}
+			},3e3)
+		}
+	}
+}// onload end
 document.body.onclick=e=>{
   if(e.target.id=='searchTerm')e.target.select()
   
   if(e.target.className=='copy'){ // COPY INFO JUST IN CASE
     navigator.clipboard.writeText(document.getElementById('ZipCode').value+'\n'+(document.getElementById('n2393619').value.length>3?document.getElementById('n2393619').value.replace(/\D/g,''):'01/01/'+(new Date().getYear()+1900-document.getElementById('n2393583').value))+'\n')
   }
+}
 
 // NC + wait/hide notif >>
 onmousedown=e=>{
@@ -64,7 +69,7 @@ document.addEventListener("paste",e=>{
 })
 // search numb format ^^^
 
-} // on load end
+//} // on load end
 
 
 
