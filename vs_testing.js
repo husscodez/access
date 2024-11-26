edb299e71609b68c2447ef5da38b396b4d2f096a
husscodez <huss.healthadvisor@gmail.com>
2024-11-25 17:23:09 -0800

    Update vs_testing.js

--- vs_testing.js
+++ vs_testing.js
@@ -4,7 +4,8 @@ function add_copy_btn(){
 document.getElementById('clientName').insertAdjacentHTML('afterend','<button class="copy" style="position:absolute;right:0px;top:10px;width:10px;padding:0px;max-width:15px;hight:10px;z-index:1;padding:4px" tabindex="0" data-accessibility-tab="true"><img class="copy" src="https://cdn-icons-png.flaticon.com/512/54/54702.png" style="width:100%;max-width:15px;margin:0px;padding:0px;hight:100%;vertical-align: middle;filter: invert(.95);"></button>')
 }
 
-document.body.onload=e=>{
+document.onload=e=>{
+  console.log("Document loaded!!!")
 if(!document.getElementsByClassName('copy').length){
   add_copy_btn()
   if(!document.getElementsByClassName('copy').length){
