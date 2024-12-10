c12f9bd5b9bca204a2c537a6010413f3006c74f0
husscodez <huss.healthadvisor@gmail.com>
2024-12-09 16:46:07 -0800

    Update RINGY.js

--- RINGY.js
+++ RINGY.js
@@ -1,4 +1,4 @@
-console.log("Ringy loaded")
+console.log("Ringy loaded...")
 
 // ***
 fetch("https://raw.githubusercontent.com/husscodez/access/main/list.json?timestamp=" + new Date().getTime())
@@ -46,6 +46,60 @@ State: ${ci.childNodes[3]?.childNodes[0]?.childNodes[0]?.childNodes[1]?.nodeValu
 Zip Code: ${(ci.childNodes[3]?.childNodes[0]?.childNodes[0]?.childNodes[1]?.nodeValue?.split(', ')[1]?.split(' ')[1])}
 Birthday: ${document.getElementsByClassName('lead-other-info-column')[0]?.innerText?.split('Birthday: ')[1]?.split('\n')[0]?.replace(/(\d{2}\/\d{2}\/)(\d{2})$/,(_,p1,p2)=>p1+(p2<50?'20':'19')+p2)}
 `)
+// **** MONDAY CODE **** >>>
+apiKey = "eyJhbGciOiJIUzI1NiJ9.eyJ0aWQiOjQ0MjkxMzA5NywiYWFpIjoxMSwidWlkIjo2OTA5MzY5NSwiaWFkIjoiMjAyNC0xMS0zMFQyMDo1MTozNi4zNzJaIiwicGVyIjoibWU6d3JpdGUiLCJhY3RpZCI6MjY3Mzc4NzcsInJnbiI6InVzZTEifQ.lulYiR65sKd3BLFJczw4kRadLh4n5qdrQYUD5gUaj8o"
+
+//M_b_id = 7954892781
+M_g_id = "new_group__1"
+M_name = ci.childNodes[0].innerText
+M_state = ci.childNodes[3]?.childNodes[0]?.childNodes[0]?.childNodes[1]?.nodeValue?.split(', ')[1]?.split(' ')[0]
+M_phone = ci.childNodes[1].innerText
+M_status = "POSITIVE RESPONSE"//"APPT SET"
+M_source = "VS DSL"
+M_dob = convertDate(document.getElementsByClassName('lead-other-info-column')[0]?.innerText?.split('Birthday: ')[1]?.split('\n')[0]?.replace(/(\d{2}\/\d{2}\/)(\d{2})$/,(_,p1,p2)=>p1+(p2<50?'20':'19')+p2))
+M_texting = "RINGY 1"
+M_zip = (ci.childNodes[3]?.childNodes[0]?.childNodes[0]?.childNodes[1]?.nodeValue?.split(', ')[1]?.split(' ')[1])
+M_email = ci.childNodes[2].innerText
+M_address = ci.childNodes[3]?.childNodes[0]?.childNodes[0]?.childNodes[0]?.innerText
+
+columnValues={
+"text__1": M_state,
+"phone__1": M_phone,
+"status": M_status,
+"status_1__1": M_source,
+"text1__1": M_dob,
+"dropdown__1": M_texting,
+"text3__1": M_zip,
+"email__1": { "email": M_email, "text": M_email },
+"text5__1": M_address
+}
+
+const query = `
+  mutation {
+    create_item (
+      board_id: "${M_b_id}", 
+      group_id: "${M_g_id}",
+      item_name: "${M_name}"
+			
+      column_values: "${JSON.stringify(columnValues).replace(/"/g, '\\"')}"
+    ) {
+      id
+    }
+  }
+`;
+
+fetch('https://api.monday.com/v2', {
+  method: 'post',
+  headers: {
+    'Content-Type': 'application/json',
+    'Authorization' : apiKey
+  },
+  body: JSON.stringify({
+    query: query
+  })
+}).then(response => response.json())
+  .then(data => console.log(data));
+// **** MONDAY CODE **** ^^^
 }
 }
 })
