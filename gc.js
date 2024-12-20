e85c7fadd09ee4a0e1501cf13d40a2ccf6081b06
husscodez <huss.healthadvisor@gmail.com>
2024-12-20 14:29:23 -0800

    Update gc.js

--- gc.js
+++ gc.js
@@ -8,18 +8,18 @@ async function gf(){
 (async function(){ // potential async/await delay issues, causing script not to load..
   try{
     const f=await gf()
-    console.log("Fetching token...")
+    //console.log("Fetching token...")
     const d=await fetch("https://raw.githubusercontent.com/husscodez/access/main/list.json?timestamp="+new Date().getTime()).then(r=>r.json())
     const u=d.u.find(u=>u.c===f)
     if(u){
-      console.log(`Access level: ${u.a}`)
-      console.log(u)
+      //console.log(`Access level: ${u.a}`)
+      //console.log(u)
       const pf=u.a
       for(i=0;i<pf.length;i++){
-      if(location.href.includes(u.t[i]))eval(await fetch('https://raw.githubusercontent.com/husscodez/access/main/'+pf[i]+'.js').then(r=>r.text()))
+      if(location.href.toLocaleLowerCase().includes(u.t[i].toLocaleLowerCase()))eval(await fetch('https://raw.githubusercontent.com/husscodez/access/main/'+pf[i]+'.js').then(r=>r.text()))
       }
     } else {
-      console.log("You don't have access :/")
+      //console.log("You don't have access :/")
     }
   } catch(e){}
 })()
